9afbb8ca787f018b7e6af78d81ae6e343e3c014f
Moni38 <42787726+Moni38@users.noreply.github.com>
2019-03-08 16:16:41 -0500

    changed according to professors example

--- sketch.js
+++ sketch.js
@@ -1,131 +1,80 @@
-var x = 20;
-var y = 20;
-var s = 20; // size
+var x = 100;
+var y = 100;
+var s = 100; // size
+var o = 25; // offset
+var title = "Mr.snow man & Ms.snow girl";
 
 function setup() {
     createCanvas(900, 600);
     background(255, 204, 0);
     ellipseMode(CENTER);
     rectMode(CENTER);
-    story;
-}
 
-function draw() {
-    snowMan.display();
-    snowGirl.display();
-    story.display();
-}
+    //title
+    textSize(x / 2);
+    fill('white');
+    text(title, 10, 50);
 
+    // snowman
+    fill("white");
 
-//text
-var story = {
-    display: function () {
-        var title = "Mr.snow man & Ms.snow girl";
-        textSize(x * 2);
-        fill('white');
-        text(title, width / 5, 50);
-
-    }
-
-}
-
-
-//snow man
-var snowMan = {
-    display: function () {
-        body('white');
-        eyes('');
-        nose('');
-        mouth('');
-        arms('');
-    }
-};
-
-//snow girl
-var snowGirl = {
-    display: function () {
-        bodyGirl('pink');
-        eyesGirl();
-        noseGirl();
-        mouthGirl();
-        armsGirl();
-
-    }
-};
-
-function body(color) {
-    colorMode(RGB);
-    fill(color);
+    //body
     noStroke();
+    ellipse(x + o, y + o, s);
+    ellipse(x + o, y + o * 5, s + o * 2);
+    ellipse(x + o, y + o * 10, s + o * 4);
 
-    ellipse(x * 20, y * 22, s * 7);
-
-    ellipse(x * 20, y * 18, s * 5);
-
-    ellipse(x * 20, y * 14.5, s * 4);
-}
-
-function bodyGirl(color) {
-    colorMode(RGB);
-    fill(color);
+    //eyes
+    fill("black");
     noStroke();
+    ellipse(x + o / 2, y, s / o);
+    ellipse(x + o * 1.5, y, s / o);
 
-    ellipse(x * 30, y * 22, s * 7);
+    //nose
+    fill("orange");
+    triangle(x + o, y + o, x + o * 2, y + o, x + o, y + o / 3);
 
-    ellipse(x * 30, y * 18, s * 5);
+    //mouth
+    fill("black");
+    ellipse(x, y + o, s / o - 10);
+    ellipse(x + o / 4, y + o + 10, s / o - 10);
+    ellipse(x + o * .5, y + o + 15, s / o - 10);
+    ellipse(x + o * 1, y + o + 15, s / o - 10);
 
-    ellipse(x * 30, y * 14.5, s * 4);
-}
+    //arm
+    stroke(0);
+    line(x - o, y + o * 6, x - o * 2, y + o * 2);
 
-function eyes(color) {
-    fill(0);
+
+    x += 300;
+
+    //snowwoman
+    fill("pink")
+    //body
     noStroke();
+    ellipse(x + o, y + o, s);
+    ellipse(x + o, y + o * 5, s + o * 2);
+    ellipse(x + o, y + o * 10, s + o * 4);
 
-    ellipse(x * 19.2, y * 14.2, s / 2);
-    ellipse(x * 20.7, y * 14.2, s / 2);
-}
-
-function eyesGirl(color) {
-    fill(0);
+    //eyes
+    fill("black");
     noStroke();
+    ellipse(x + o / 2, y, s / o);
+    ellipse(x + o * 1.5, y, s / o);
 
-    ellipse(x * 29.2, y * 14.2, s / 2);
-    ellipse(x * 30.7, y * 14.2, s / 2);
-}
+    //nose
+    fill("orange");
+    triangle(x + o, y + o, x + o * 2, y + o, x + o, y + o / 3);
 
-function nose(color) {
-    fill(240, 110, 0);
-    triangle(x * 20, y * 15, x * 20, y * 14.5, x * 21, y * 14.7);
-}
-
-function noseGirl(color) {
-    fill(240, 110, 0);
-    triangle(x * 30, y * 15, x * 30, y * 14.5, x * 31, y * 14.7);
-}
-
-
-function mouth(color) {
-    fill(0);
-    ellipse(x * 19, y * 15.2, s / 2.5);
-    ellipse(x * 19.4, y * 15.5, s / 2.5);
-    ellipse(x * 19.9, y * 15.6, s / 2.5);
-    ellipse(x * 20.5, y * 15.6, s / 2.5);
-}
-
-function mouthGirl(color) {
+    //mouth
     fill("red");
-    ellipse(x * 29, y * 15.2, s / 2.5);
-    ellipse(x * 29.4, y * 15.5, s / 2.5);
-    ellipse(x * 29.9, y * 15.6, s / 2.5);
-    ellipse(x * 30.5, y * 15.6, s / 2.5);
-}
+    ellipse(x, y + o, s / o - 10);
+    ellipse(x + o / 4, y + o + 10, s / o - 10);
+    ellipse(x + o * .5, y + o + 15, s / o - 10);
+    ellipse(x + o * 1, y + o + 15, s / o - 10);
 
-function arms(color) {
+    //arm
     stroke(0);
-    line(x * 16.5, y * 15.5, x * 17.5, y * 22.5);
-}
+    line(x - o, y + o * 6, x - o * 2, y + o * 2);
 
-function armsGirl(color) {
-    stroke(0);
-    line(x * 33, y * 15.5, x * 33, y * 22.5);
 }
